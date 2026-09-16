3ee6d86e362c20282debd2c6a0ff6d30be13b0dc
masabacarlm <masabacarl8@gmail.com>
2026-09-16 16:10:12 +0300

    Add mark management

--- main.py
+++
@@ -1,6 +0,0 @@
-def add_mark(self, mark):
-    if mark >= 0 and mark <= 100:
-        self.marks.append(mark)
-        print(mark, "has been added successfully.")
-    else:
-        print("Invalid mark. Mark must be between 0 and 100.")
