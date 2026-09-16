8de59b7e9f089c302931eaeb6ff61cc98d8c253a
DAMBA001 <alexkwagala09@gmail.com>
2026-09-16 16:02:23 +0300

    Add Student class structure

---
+++ student.py
@@ -0,0 +1,38 @@
+# Group Members:
+# 1. Member Name
+# 2. Member Name
+# 3. Member Name
+# 4. Member Name
+# 5. Member Name
+# 6. Member Name
+
+
+class Student:
+    def __init__(self, name, registration_number):
+        self.name = name
+        self.registration_number = registration_number
+        self.marks = []
+
+    def add_mark(self, mark):
+        # Member 2
+        self.marks.append(mark)
+
+    def calculate_average(self):
+        # Member 3
+        pass
+
+    def get_grade(self):
+        # Member 4
+        pass
+
+    def has_passed(self):
+        # Member 5
+        pass
+
+    def display_results(self):
+        # Member 6
+        pass
+
+
+# SHORT DEMONSTRATION
+# Objects will be created here after the class is completed.
