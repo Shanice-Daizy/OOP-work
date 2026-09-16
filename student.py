435d87fe704299d585fcf03da75d90f842108ae5
mac <mac@shan.local>
2026-09-16 16:45:40 +0300

    Update grade calculation in student class

---
+++ student.py
@@ -0,0 +1,13 @@
+def get_grade(self):
+    average = self.calculate_average()
+
+    if average >= 80:
+        return "A"
+    elif average >= 70:
+        return "B"
+    elif average >= 60:
+        return "C"
+    elif average >= 50:
+        return "D"
+    else:
+        return "F"
